4375cab4f365e88f6e74325beed98265e4b92cfc
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-10-24 02:59:32 +0000

    automatically backed up by learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -12,14 +12,10 @@ export default class DigitalClicker extends React.Component{
   }
   render(){
     return(
-<<<<<<< HEAD
       <button onClick={this.handleClick}>
         {this.timeCliked}
       </button>
-=======
       <button onClick={this.handleClick}>{this.timeCliked}</button>
->>>>>>> e79f40db5ae55ab4a4b77333cf2b3cfb16c23dd4
-
     )
   }
 }
