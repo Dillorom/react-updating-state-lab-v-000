63760a8f661d6f5a947b5b0b9c4d510162aa86a4
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-10-24 03:13:28 +0000

    automatically backed up by learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -4,38 +4,38 @@ export default class YouTubeDebugger extends React.Componetn{
   constructor(){
     super()
     this.state = {
-      errors: [], user: null, settings: { bitrate: 8, video: { resolution: '1080p' } }
-
-    }
-  }
-  handleBitrate = () => (
-    this.setState({
-      js: {
+      errors: [], 
+      user: null, 
       settings: {
-      ...this.state.settings,
-      bitrate: 12
-    }
-  }
-    })
-  )
-  handleResoluton = () => (
-    this.setState({
-      js: {
-        settings: {
-          video: {
-            ...this.state.video,
-            resoluton: '720p'
-
-          }
-        }
+        bitrate: 8,
+         video: {
+            resolution: '1080p' 
+          } 
       }
+    };
+  }
+  handleBitrate = () => {
+    this.setState({
+        settings: Object.assign({}, this.state.settings, {
+          bitrate: 12
+        }),
+      });
+  }
+  
+  handleResoluton = () =>    {
+    this.setState({
+      settings: Object.assign({}, this.state.settigs, {
+        video: Object.asign({}, this.state.settings.video, {
+          resolution: '720p'
+        })
+      })
     })
-  )
+  }
 
   render(){
     return(
-      <button className="bitrate" onClick={this.handleBitrate}></button>
-      <button className="resoluton" onClick-{this.handleResoluton}></button>
+      <button className="bitrate" onClick={this.handleBitrate}>Change Bitrate</button>
+      <button className="resoluton" onClick-{this.handleResoluton}>Change Resolution</button>
 
     )
   }
